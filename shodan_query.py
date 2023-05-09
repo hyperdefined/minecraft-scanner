47fab211926c5df588981324f19dc3e235f6ca75
hyperdefined <contact@hyper.lol>
2023-05-09 00:17:33 -0400

    more changes
    detects forge, saves icon
    moved updating data to different file

--- shodan_query.py
+++ shodan_query.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import time
 from dotenv import load_dotenv
+import single_query
+import sys
 
 def motd_to_string(motd):
     # If the dictionary only has a 'text' key, return its value
@@ -32,16 +34,31 @@ api_key = os.getenv('SHODAN_KEY')
 api = shodan.Shodan(api_key)
 
 data_exists = os.path.exists("database.csv")
-minecraft_server_data = pd.DataFrame(columns=['IP','Port','Protocol','Software','MOTD'])
+minecraft_server_data = pd.DataFrame(columns=['IP','Port','Protocol','Software','MOTD', 'Forge', 'Icon'])
 if data_exists:
     print('Loading current database...')
     minecraft_server_data = pd.read_csv("database.csv")
+    print('Loaded ' + str(len(minecraft_server_data.index)) + " servers.")
+
+counter = 0
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == 'update':
+        saved_ips = minecraft_server_data['IP']
+        for ip in saved_ips:
+            single_query.update_server_info(ip_address=ip)
+            time.sleep(5)
 
 try:
-    results = api.search("\"Minecraft Server:\" ")
+    results = api.search("\"Minecraft Server:\"")
     for result in results["matches"]:
         ip_address = result['ip_str']
         port = result['port']
+
+        # if we have the IP saved, ignore it and continue
+        already_saved = (minecraft_server_data['IP'] == ip_address).any()
+        if already_saved:
+            continue
 
         # only continue if it's a minecraft server
         if 'minecraft' not in result:
@@ -51,35 +68,48 @@ try:
         description = minecraft_info['description']
         protocol, server_software = minecraft_info['version']['protocol'], minecraft_info['version']['name']
 
-        # check to see if we have it saved already
-        ip_query = minecraft_server_data[minecraft_server_data['IP'] == ip_address]
-        # update the data
-        if len(ip_query) > 0:
-            print('Updating data for IP ' + ip_address)
-            for index, row in ip_query.iterrows():
-                minecraft_server_data.at[index, 'Port'] = port
-                minecraft_server_data.at[index, 'Protocol'] = protocol
-                minecraft_server_data.at[index, 'Software'] = server_software
-                minecraft_server_data.at[index, 'MOTD'] = description
-            continue    
+        # track if the server is using forge
+        is_forge = 0
+        if 'forgeData' in minecraft_info:
+            is_forge = 1
+
+        # see if the server has an icon set
+        icon = "none"
+        if 'favicon' in minecraft_info:
+            icon = minecraft_info['favicon']
 
         print('Adding ' + ip_address)
-        new_server_data = {'IP':ip_address, 'Port':port, 'Protocol':protocol, 'Software':server_software, 'MOTD': motd_to_string(description)}
+        counter = counter + 1
+
+        # build the data to add
+        new_server_data = {'IP':ip_address, 'Port':port, 'Protocol':protocol, 'Software':server_software, 'MOTD': motd_to_string(description), 'Forge': is_forge, 'Icon': icon}
+        
+        # add the new data
         new_row_df = pd.DataFrame([new_server_data])
         minecraft_server_data = pd.concat([minecraft_server_data, new_row_df], ignore_index=True)
 except shodan.APIError as e:
     print("Error: %s" % e)
 
 minecraft_server_data.to_csv("database.csv", index=False)
+
+if counter > 0:
+    print('Added ' + str(counter) + ' new servers.')
+    print('New total: ' + str(len(minecraft_server_data.index)) + ' servers.')
+
+# only join servers from 1.8 to 1.19 since that's what Mineflayer supports
+# filter out forge servers since we can't join them
 mc_versions = [47, 107, 108, 109, 110, 210, 315, 316, 335, 338, 340, 393, 401, 404, 477, 480, 485, 490, 498, 573, 575, 578, 735, 736, 751, 753, 754, 755, 756, 757, 758, 759]
-servers = minecraft_server_data[minecraft_server_data['Protocol'].isin(mc_versions)]
+servers = minecraft_server_data[(minecraft_server_data['Protocol'].isin(mc_versions)) & (minecraft_server_data['Forge'] == 0)]
+# randomize the rows for fun
 servers = servers.sample(frac=1, random_state=42)
 for index, row in servers.iterrows():
     ip_address = row['IP']
     port = row['Port']
     version = row['Protocol']
+    # build the command to run the node script to join
     print('Trying ' + str(ip_address) + ':' + str(port) + ' with version ' + str(version))
     cmd = ['node', 'join_server.js', str(ip_address), str(port)]
     result = subprocess.Popen(cmd)
     result.wait()
+    # sleep for 1 minute so we don't ratelimit ourself
     time.sleep(60)
---
+++ single_query.py
@@ -0,0 +1,63 @@
+import shodan
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+def update_server_info(ip_address):
+    load_dotenv()
+    api_key = os.getenv('SHODAN_KEY')
+    api = shodan.Shodan(api_key)
+
+    data_exists = os.path.exists("database.csv")
+    minecraft_server_data = pd.DataFrame(columns=['IP','Port','Protocol','Software','MOTD', 'Forge', 'Icon'])
+    if data_exists:
+        minecraft_server_data = pd.read_csv("database.csv")
+    else:
+        print('Unable to load database!')
+        return
+
+    try:
+        results = api.host(ip_address)
+        minecraft_data = None
+        port = None
+        # go through the data shodan has
+        # check for Minecraft data
+        for data in results['data']:
+            if 'minecraft' not in data:
+                continue
+            minecraft_data = data['minecraft']
+            port = data['port']
+
+        # if there is no Minecraft data, exit
+        if minecraft_data == None:
+            print('IP address has no Minecraft data attached.')
+            return
+        
+        description = minecraft_data['description']
+        protocol, server_software = minecraft_data['version']['protocol'], minecraft_data['version']['name']
+
+        # track if the server is using forge
+        is_forge = 0
+        if 'forgeData' in minecraft_data:
+            is_forge = 1
+
+        # see if the server has an icon set
+        icon = "none"
+        if 'favicon' in minecraft_data:
+            icon = minecraft_data['favicon']
+
+        ip_query = minecraft_server_data[minecraft_server_data['IP'] == ip_address]
+        # update the data
+        if len(ip_query) > 0:
+            print('Updating data for IP ' + ip_address)
+            for index, row in ip_query.iterrows():
+                minecraft_server_data.at[index, 'Port'] = port
+                minecraft_server_data.at[index, 'Protocol'] = protocol
+                minecraft_server_data.at[index, 'Software'] = server_software
+                minecraft_server_data.at[index, 'MOTD'] = description
+                minecraft_server_data.at[index, 'Forge'] = is_forge
+                minecraft_server_data.at[index, 'Icon'] = icon   
+    except shodan.APIError as e:
+        print("Error: %s" % e)
+
+    minecraft_server_data.to_csv("database.csv", index=False)
